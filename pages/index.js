73fa281049c6b93398d75c63919d2ee5d7e5841f
pedronascr <pedrohnrosa@gmail.com>
2026-02-12 16:14:06 +0000

    TESTE IMAGEM LOCAL

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <img src="https://tse2.mm.bing.net/th/id/OIP.dnVupN1QHLwm69ORI8mICgHaJ4?rs=1&pid=ImgDetMain&o=7&rm=3" alt="Descrição" />
+    return <img src="/images/logo site teste.png" alt="Descrição" />
 }
 
 export default Home;

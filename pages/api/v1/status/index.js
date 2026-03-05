af529b3580b15de1e587395f6bb12a90af41cee1
pedronascr <pedrohnrosa@gmail.com>
2026-03-05 14:09:25 +0000

    Edição dos testes e página de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,19 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ chave: "OK" });
+  const updatedAt = new Date().toISOString();
+  const maxConnections = await database.query("SHOW max_connections;");
+  const activeConnections = await database.query(
+    "SELECT count(*) FROM pg_stat_activity;",
+  );
+  const actualVersion = await database.query("SELECT version();");
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    max_connections: maxConnections,
+    active_connections: activeConnections,
+    actual_version: actualVersion,
+  });
 }
 
 export default status;

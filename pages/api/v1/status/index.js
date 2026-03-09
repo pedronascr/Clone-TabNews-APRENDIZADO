ae12b0d73314e8633ff49befd18acaffd7d11f70
pedronascr <pedrohnrosa@gmail.com>
2026-03-09 12:06:37 +0000

    Finalização do arquivo de endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,13 +6,32 @@ async function status(req, res) {
   const activeConnections = await database.query(
     "SELECT count(*) FROM pg_stat_activity;",
   );
-  const actualVersion = await database.query("SELECT version();");
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   res.status(200).json({
     updated_at: updatedAt,
-    max_connections: maxConnections.rows[0].max_connections,
-    active_connections: activeConnections.rows[0].count,
-    actual_version: actualVersion.rows[0].version,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
